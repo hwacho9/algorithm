1a9732992ceb4c84bcdfefeb727d9a3f90474236
hwacho9 <csh77776@gmail.com>
2025-01-13 23:43:20 +0900

    baekjoon/11021.cpp

---
+++ baekjoon-c++/11021.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int t;
+
+    std::cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        std::cout << "Case #" << i << ": " << a + b << '\n';
+    }
+}
