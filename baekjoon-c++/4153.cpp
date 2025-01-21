dd13c4db8f64a28c2db2d52ae00c1e2c5bb93f31
hwacho9 <csh77776@gmail.com>
2025-01-21 23:59:34 +0900

    4153.cpp

---
+++ baekjoon-c++/4153.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int array[3];
+
+    while (1)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> array[i];
+        }
+
+        sort(array, array + 3);
+
+        if (array[0] * array[0] + array[1] * array[1] == array[2] * array[2])
+        {
+            cout << "right" << endl;
+        }
+        else
+        {
+            cout << "wrong" << endl;
+        }
+
+        if (array[0] == 0 || array[1] == 0 || array[2] == 0)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
